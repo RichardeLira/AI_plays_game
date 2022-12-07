db4f780461bcc7c5cef7162d1ef0312000904d27
Pedro Grigorio <pedrooh.hxh@gmail.com>
2022-12-07 14:02:14 -0300

    predict script

--- play.py
+++ play.py
@@ -1,9 +1,25 @@
 import cv2
 import time
+import pytorch
 import keyboard
 import numpy as np
 import pydirectinput
 from PIL import ImageGrab
+
+
+# device
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+# load model
+model = torch.load(Config.MODEL_PATH).to(Config.DEVICE)
+model.eval()
+
+# define normalization transforms
+transforms = transforms.Compose(
+    [transforms.ToPILImage(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean= Config.MEAN, std=Config.STD)]
+)
 
 time.sleep(5)
 label = [0,0,0,0]
@@ -11,10 +27,22 @@ while True:
 
     # print screen
     screenshot = cv2.cvtColor(np.array(ImageGrab.grab(bbox=(450,160,1470,875)), dtype='uint8'), cv2.COLOR_BGR2RGB)
-    screenshot = cv2.resize(screenshot, (299, 299))
 
     # show prints
     cv2.imshow('Video', screenshot)
+
+    # resize image and transpose layot for pytorch
+    screenshot = cv2.resize(screenshot, (299, 299))
+    screenshot = screenshot.transpose((2, 0, 1))
+
+    # convert image to tensor and normalize it
+    screenshot = torch.from_numpy(screenshot)
+	screenshot = transforms(screenshot).to(Config.DEVICE)
+	screenshot = screenshot.unsqueeze(0)
+
+    # predict the labels
+    label = model(screenshot)
+    label2 = torch.nn.Softmax(dim=-1)(label)
 
     # key press
     if(label[0] == 1):
